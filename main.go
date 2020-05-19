e6195315f18a5563f1df87169a777b7946effbe5
azak <azak@protonmail.com>
2020-05-19 15:13:39 +0200

    Added Check Method and Reverse Method
    * added Check Method which checks if a file is a symlink
    * added Reverse Method which moves the symlink file to the file

--- main.go
+++ main.go
@@ -1,7 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	ERROR_BIND      = "ERROR: Could not bind: "
+	ERROR_PARSE     = "ERROR: while parsing: "
+	ERROR_READ_FLAG = "ERROR: while retrieving: "
+	ERROR_READ_FILE = "ERROR: while reading File: "
+	ERROR_PATH_DIR  = "ERROR: while reading Path is Directory"
+	ERROR_NOT_LINK  = "ERROR: file is not a link"
+)
+
+func ReadFile(args []string) (*string, error) {
+	fileFlag := pflag.NewFlagSet("file", pflag.ContinueOnError)
+	fileFlag.String("file", "", "the file for which the link should be reversed")
+	err := viper.BindPFlags(pflag.CommandLine)
+	if err != nil {
+		log.Println(ERROR_BIND, err)
+		return nil, err
+	}
+
+	err = fileFlag.Parse(args)
+	if err != nil {
+		log.Println(ERROR_PARSE, err)
+		return nil, err
+	}
+
+	fileName, err := fileFlag.GetString("file")
+	if err != nil {
+		log.Println(ERROR_READ_FLAG, err)
+		return nil, err
+	}
+	log.Println("Reading file: ", fileName)
+
+	stat, err := os.Stat(fileName)
+	if err != nil {
+		log.Println(ERROR_READ_FILE, err)
+		return nil, err
+	}
+
+	lstat, err := os.Lstat(fileName)
+	if err != nil {
+		log.Println(ERROR_READ_FILE, err)
+		return nil, err
+	}
+
+	if stat.IsDir() || lstat.IsDir() {
+		log.Println(ERROR_PATH_DIR)
+		return nil, errors.New(ERROR_PATH_DIR)
+	}
+
+	if lstat.Mode()&os.ModeSymlink != os.ModeSymlink {
+		log.Println(ERROR_NOT_LINK)
+		return nil, errors.New(ERROR_NOT_LINK)
+	}
+
+	return &fileName, nil
+}
+
+func ReverseFile(path string) error {
+
+	p, err := filepath.EvalSymlinks(path)
+	if err != nil {
+		return err
+	}
+
+	err = os.Rename(p, path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("vim-go")
+	_, err := ReadFile(os.Args)
+	if err != nil {
+		panic("ERROR: " + err.Error())
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,7 +1,74 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func createSimlinks(t *testing.T) {
+	if _, err := os.Lstat("./random.json"); err == nil {
+		os.Remove("./random.json")
+	}
+	_, err := os.Lstat("./random.json")
+	require.Error(t, err)
+
+	if _, err := os.Lstat("./test/random.json"); err != nil {
+		d := []byte("{\n\t\"test\": \"testvalue\"\n}")
+		err := ioutil.WriteFile("./test/random.json", d, 0644)
+		require.NoError(t, err)
+	}
+
+	err = os.Symlink("./test/random.json", "./random.json")
+	require.NoError(t, err)
+}
+
+func TestReadFile(t *testing.T) {
+	fmt.Println("testing: TestReadFile")
+	createSimlinks(t)
+
+	data, err := ioutil.ReadFile("./random.json")
+	require.NoError(t, err)
+	require.NotZero(t, data)
+
+	data, err = ioutil.ReadFile("./test/random.json")
+	require.NoError(t, err)
+	require.NotZero(t, data)
+
+	strings := []string{"--file=./test/random.json"}
+	fileName, err := ReadFile(strings)
+	assert.Error(t, err)
+	assert.Nil(t, fileName)
+
+	strings = []string{"--file=./random.json"}
+	fileName, err = ReadFile(strings)
+	assert.NoError(t, err)
+	require.NotNil(t, fileName)
+	assert.Equal(t, *fileName, "./random.json")
+}
+
+func TestReverseFile(t *testing.T) {
+	fmt.Println("testing: TestReverseFile")
+	createSimlinks(t)
+	require.FileExists(t, "./random.json")
+	require.FileExists(t, "./test/random.json")
+	Lstat, err := os.Lstat("./random.json")
+	require.NoError(t, err)
+	require.False(t, Lstat.Mode()&os.ModeSymlink != os.ModeSymlink)
+
+	err = ReverseFile("./random.json")
+	assert.NoError(t, err)
+
+	Lstat, err = os.Lstat("./random.json")
+	require.NoError(t, err)
+	require.True(t, Lstat.Mode()&os.ModeSymlink != os.ModeSymlink)
+	_, err = os.Lstat("./test/random.json")
+	require.Error(t, err)
+
+	err = os.Remove("./random.json")
+	require.NoError(t, err)
 }
